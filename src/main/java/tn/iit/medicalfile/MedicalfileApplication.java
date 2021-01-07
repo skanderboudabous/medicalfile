f05a019d4b6ec6adc48938c82b6689dbf7a8bfef
Skander <Skander@LAPTOP-PPK2F1IK>
2021-01-07 23:00:37 +0100

    first commit(config)

---
+++ src/main/java/tn/iit/medicalfile/MedicalfileApplication.java
@@ -0,0 +1,13 @@
+package tn.iit.medicalfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalfileApplication.class, args);
+    }
+
+}
---
+++ src/main/java/tn/iit/medicalfile/config/Links.java
@@ -0,0 +1,8 @@
+package tn.iit.medicalfile.config;
+
+public class Links {
+    public static final String STORE_URL ="http://localhost:8085/store-management/api/";
+    public static final String MEDICAMENTS=STORE_URL+"medicaments";
+    public static final String CATEGORIES=STORE_URL+"categories";
+
+}
